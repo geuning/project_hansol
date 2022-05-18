89448ed7b583097211da49a80533ce7bcefdac60
thethe133 <thethe133@naver.com>
2022-05-18 21:49:04 +0900

    refactor: springdoc-openapi 적용
    - springfox -> springdoc-openapi 변경
    - swagger v2 -> v3 변경

--- src/main/java/com/example/project_hansol/api/member/controller/MemberController.java
+++ src/main/java/com/example/project_hansol/api/member/controller/MemberController.java
@@ -3,6 +3,8 @@ package com.example.project_hansol.api.member.controller;
 import com.example.project_hansol.api.member.dto.MemberDto;
 import com.example.project_hansol.domain.member.model.Member;
 import com.example.project_hansol.domain.member.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,12 +14,14 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@Tag(name="MemberController", description = "회원 API")
 @RequestMapping("/api/members")
 @RequiredArgsConstructor
 public class MemberController {
 
     private final MemberService memberService;
 
+    @Operation(summary = "전체 회원 조회", description = "전체 회원을 조회합니다.")
     @GetMapping
     public List<MemberDto> findAllMembers() {
         List<Member> allMembers = memberService.findAllMembers();
